7cb542685036d160f3165282732e4232ced5fddd
uhyo <uhyo@uhy.ooo>
2017-07-16 00:17:46 +0900

    move option parsing into lib

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,20 @@
-extern crate getopts;
+extern crate othello;
 
 use std::env;
 use std::io;
 use std::io::Write;
 use std::process;
-use std::error::Error;
+
+use othello::options;
 
 fn main() {
-    let opts = options().unwrap_or_else(|err| {
+    let args: Vec<String> = env::args().collect();
+    let opts = options::parse(args).unwrap_or_else(|err| {
         writeln!(
             io::stderr(),
-            "{}", err);
+            "{}", err).unwrap();
         process::exit(1);
     });
 
-    println!("port: {}", opts.port);
+    println!("{:?}", opts);
 }
-
-struct Opts {
-    port: i32,
-}
-
-fn options() -> Result<Opts, String>{
-    // Get arguments
-    let args: Vec<String> = env::args().collect();
-
-    // Define options
-    let mut opts = getopts::Options::new();
-    opts.reqopt("p", "port", "Port of othello server.", "PORT");
-
-    let args = try!(opts.parse(args).or_else(geterr));
-
-    let port = args.opt_str("port").unwrap();
-    let port = try!(port.parse::<i32>().or_else(geterr));
-
-    Ok(Opts {
-        port,
-    })
-}
-
-fn geterr<T: Error, S>(err: T) -> Result<S, String>{
-    Err(err.description().to_owned())
-}
-
---
+++ src/options.rs
@@ -0,0 +1,36 @@
+// options: parse options.
+extern crate getopts;
+
+use std::error::Error;
+
+#[derive(Debug)]
+pub struct Opts {
+    port: i32,
+    host: String,
+}
+
+pub fn parse(args: Vec<String>) -> Result<Opts, String>{
+
+    // define options
+    let mut opts = getopts::Options::new();
+    opts.reqopt("p", "port", "Port of othello server.", "PORT");
+    opts.optopt("h", "host", "Host of othello server.", "HOST");
+
+    // parse options
+    let args = try!(opts.parse(args).or_else(geterr));
+
+    // get options
+    let port = args.opt_str("port").unwrap();
+    let port = try!(port.parse::<i32>().or_else(geterr));
+    let host = args.opt_str("host").unwrap_or(String::from("localhost"));
+
+    Ok(Opts {
+        port,
+        host,
+    })
+}
+
+fn geterr<T: Error, S>(err: T) -> Result<S, String>{
+    Err(err.description().to_owned())
+}
+
